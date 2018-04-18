6fac439d07503515844be8d90e55e3a3292adc90
caoyan <caoyan@bbdservice.com>
2018-04-18 14:19:11 +0800

    add render

--- Enzyme-test/test/index2.test.js
+++ Enzyme-test/test/index2.test.js
@@ -1,20 +1,29 @@
-import { assert } from 'chai';
+import {assert} from 'chai';
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Index from '../src/Index2';
 
-const { shallow, mount } = Enzyme;
+const {shallow, mount, render} = Enzyme;
 
 Enzyme.configure({
     adapter: new Adapter()
 });
 
 describe('Enzyme的浅渲染测试套件', () => {
-    it('Example组件中按钮的名字为text的值', () => {
+    it('Index组件中按钮的名字为text的值', () => {
         const name = '按钮';
         let app = mount(<Index text={name}/>);
-        assert.equal(app.find('button').text(). name);
-        assert.equal(app.find('p').text(). name);
+        assert.equal(app.find('button').text().name);
+        assert.equal(app.find('p').text().name);
     });
+});
+
+describe('Enzyme render的静态HTML渲染（Static Rendered Markup）中', function () {
+    it('Index组件中按钮的名字为子组件Sub中p的值', function () {
+        const name = '按钮名';
+        let app = render(<Index text={name}/>);
+        assert.equal(app.find('button').text().name);
+        assert.equal(app.find('p').text().name);
+    })
 });
