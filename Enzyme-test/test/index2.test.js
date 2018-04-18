28b1b05f6e380075c933c34b3c61dd26c3bb0766
caoyan <caoyan@bbdservice.com>
2018-04-18 14:46:59 +0800

    update

--- Enzyme-test/test/index2.test.js
+++ Enzyme-test/test/index2.test.js
@@ -4,7 +4,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Index from '../src/Index2';
 
-const {shallow, mount, render} = Enzyme;
+const { shallow, mount, render } = Enzyme;
 
 Enzyme.configure({
     adapter: new Adapter()
@@ -14,8 +14,7 @@ describe('Enzyme的浅渲染测试套件', () => {
     it('Index组件中按钮的名字为text的值', () => {
         const name = '按钮';
         let app = mount(<Index text={name}/>);
-        assert.equal(app.find('button').text().name);
-        assert.equal(app.find('p').text().name);
+        assert.equal(app.find('button').text(), app.find('p').text());
     });
 });
 
@@ -23,7 +22,6 @@ describe('Enzyme render的静态HTML渲染（Static Rendered Markup）中', func
     it('Index组件中按钮的名字为子组件Sub中p的值', function () {
         const name = '按钮名';
         let app = render(<Index text={name}/>);
-        assert.equal(app.find('button').text().name);
-        assert.equal(app.find('p').text().name);
+        assert.equal(app.find('button').text(), app.find('p').text());
     })
 });
